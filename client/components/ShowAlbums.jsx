19aa92eaef8b9a65720c9c70f5180155fb8f5d06
Alvaro <98783170+alvaro-hudson@users.noreply.github.com>
2022-08-05 14:35:39 +1200

    Added basic CSS to showAlbums. Added function to show/hide buttons on mouseenter & leave but does it for all of them and not individually

--- client/components/ShowAlbums.jsx
+++ client/components/ShowAlbums.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { fetchAlbums } from '../actions/index'
 import { useSelector, useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
@@ -11,28 +11,37 @@ function ShowAlbums() {
 
 const dispatch = useDispatch()
 
+const [options, showOptions] = useState(false)
+
   useEffect(() => {
     return dispatch(fetchAlbums())
   },[])
 
   const albums = useSelector(state => state.albums)
 
+  const handleMouseEnter = () => {
+    showOptions(true)
+  }
+
+  const handleMouseLeave = () => {
+    showOptions(false)
+  }
+
   return (
     <>
       <Header/>
-      <div className='show-albums redux-thunk'>
+      <div className='show-albums'>
         {albums.map(data => {
-          console.log('data', data)
           return (
-          <>
-            <h2>Album: {data.album}</h2>
-            <ul key={data.id}>
+            <div key={data.id} className='album-info' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+              <h2>Album: {data.album}</h2>
+              <ul key={data.id}>
               <li>Artist: {data.artist}</li>
               <li>Year: {data.year}</li>
-            </ul>
-            <DeleteAlbum id={data.id}/>
-            <UpdateAlbum data={data}/>
-          </>
+              </ul>
+              {options && <DeleteAlbum id={data.id}/>}
+              {options && <UpdateAlbum data={data}/>}
+            </div>
         )})}
         <Link to="/addalbum"><button>Add a new album</button></Link>
       </div>
